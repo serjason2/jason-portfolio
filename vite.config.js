7f8343f82b1b8546679d1cbf2b73858948785276
Jason Sourivong <serjason2@gmail.com>
2025-04-06 18:08:02 -0500

    configs configured

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/react-website",
+  base: "/jason-portfolio",
 });
